98c89a5ab7ad2929e4ed6be96c287e695021a0b6
Utkarsh_patel <utkarshpatel@cedcoss.com>
2022-02-05 11:22:39 +0530

    Multiply Two Numbers with JavaScript

--- src/assignment4.js
+++ src/assignment4.js
@@ -39,5 +39,7 @@ const sum = 10 + 10;  //Add Two Numbers with JavaScript
 
 const difference = 45 - 33;//Subtract One Number from Another with JavaScript
 
+const product = 8 * 10; //Multiply Two Numbers with JavaScript
+
 
 
