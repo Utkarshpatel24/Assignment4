4ea165d9b44472a7098da536032c68aa923c476d
Utkarsh_patel <utkarshpatel@cedcoss.com>
2022-02-05 14:42:08 +0530

    Understanding Boolean Values

--- src/assignment4.js
+++ src/assignment4.js
@@ -273,3 +273,9 @@ const testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+
+
+function welcomeToBooleans() { //Understanding Boolean Values
+    return true; 
+   }
